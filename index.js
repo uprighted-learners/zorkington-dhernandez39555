864993b18485ec2674a30c2f6321f4c62d750d8c
Dante Hernandez <hernandezdante2001@gmail.com>
2023-06-12 19:51:31 -0400

    items and inventories, starting on reading

--- index.js
+++ index.js
@@ -26,17 +26,17 @@ On the door is a handwritten sign.\n`;
 
 //?   Tu l'as!!! Optimisme est le premier regle
 
-//maybe hand draw a wire frame to help conceptualize everything
-// I need a state for all the rooms to be in
+//maybe hand draw a wire frame to help conceptualize everything //!check
+// I need a state for all the rooms to be in //!check
 //each class should be a name of an item in the state to do the following:
-//into an inventory, a description, and a connection
-//a player state, with an updatable array and a status 
-//status= location?
+//into an inventory, a description, and a connection //!check, not, check
+//player location //!Check need to display w/ command
 //create a read function
-//create a take function
-//create a drop function
+//create a take function //! Check
+//create a drop function //! Check
 //create a keypad function and a class to go with
 
+//! Room Changing Functions
 let Rooms = {
   firstRoom:["secondRoom", "fourthRoom"],
   secondRoom:["firstRoom","thirdRoom","fifthRoom"],
@@ -51,39 +51,80 @@ function roomChanger(newRoom){
   let validTransition= Rooms[currentRoom]
   if (validTransition.includes(newRoom)){
     currentRoom = newRoom
-    console.log(`You have entered ${currentRoom}`)
+    console.log(`You have entered ${currentRoom}`)//todo change into running root function
   }
 }
+let playerLocation = currentRoom;
+  //!Room Changing Functions
 
-class takeableItems {
-  constructor(isTakeable,isInRoom,){
-    this.isTakeable=isTakeable
-    this.isInRoom=isInRoom
-  }
-}
+
+//! Item Functions/Works
 
 let charInventory = [];
-let playerLocation = currentRoom;
-let globalItemArray = ["Knife"]
+//Room-based item inventories
+firstRoomItemArray=["jagged-note"]; //puzzle item is behind main sign
+secondRoomItemArray=["butcher's-knife"];//puzzle item is a number inscribed on the handle
+thirdRoomItemArray=["hint-note"]; //puzzle number is found from reading the hint and using the room description to spell out the word
+fourthRoomItemArray=[];
+fifthRoomItemArray=[];
+sixthRoomItemArray=[];
 
-function collectItem(item){
-  if(globalItemArray.includes(item)){
-    if(item.isTakeable && item.isInRoom){
-      charInventory.push()
-      return `You have taken the ${item}`
-    } else if (item.isTakeable && !item.isInRoom){
-      return `${item} is not in this room`
-    } else if (item.isInRoom && !item.isTakeable){
-      return `${item} is not able to be taken`
+
+
+function itemCollection(item){
+  if (!firstRoomItemArray.includes(item) && !secondRoomItemArray.includes(item) && !thirdRoomItemArray.includes(item) && !fourthRoomItemArray.includes(item) && !fifthRoomItemArray.includes(item)){
+    console.log(`The ${item} does not exist`)
+  } else{
+    if(playerLocation=Rooms.firstRoom && firstRoomItemArray.includes(item)){
+      charInventory.push(item)
+      firstRoomItemArray.splice([item],1)
+    } else if(playerLocation=Rooms.secondRoom && secondRoomItemArray.includes(item)){
+      charInventory.push(item)
+      secondRoomItemArray.splice([item],1)
+    } else if(playerLocation=Rooms.thirdRoomRoom && thirdRoomItemArray.includes(item)){
+      charInventory.push(item)
+      thirdRoomItemArray.splice([item],1)
+    } else if(playerLocation=Rooms.fourthRoom && fourthRoomItemArray.includes(item)){
+      charInventory.push(item)
+      fourthRoomItemArray.splice([item],1)
+    } else if(playerLocation=Rooms.fifthRoom && fifthRoomItemArray.includes(item)){
+      charInventory.push(item)
+      fifthRoomItemArray.splice([item],1)
+    } else if(playerLocation=Rooms.sixthRoom && sixthRoomItemArray.includes(item)){
+      charInventory.push(item)
+      sixthRoomItemArray.splice([item],1)
     } else {
-      return `${item} does not exist `
+      console.log(`${item} is not in this room`)
     }
-  } else {
-    return `${item} does not exist `
   }
 }
-
-function displayInventory(){
+function itemDroppage(item){
+  if(playerLocation=firstRoom){
+    firstRoomItemArray.push(item)
+    charInventory.splice([item],1)
+  } else if(playerLocation=secondRoom){
+    secondRoomItemArray.push(item)
+    charInventory.splice([item],1)
+  } else if(playerLocation=thirdRoom){
+    thirdRoomItemArray.push(item)
+    charInventory.splice([item],1)
+  } else if(playerLocation=fourthRoom){
+    fourthRoomItemArray.push(item)
+    charInventory.splice([item],1)
+  } else if(playerLocation=fifthRoom){
+    fifthRoomItemArray.push(item)
+    charInventory.splice([item],1)
+  } else {
+    sixthRoomItemArray.push(item)
+    charInventory.splice([item],1)
+  } 
+}
+function seeCharacterInventory(){
   console.log(charInventory)
 }
-roomChanger("secondRoom")
+function readItem(item){
+  if(e){}
+}
+//! Item Functions/ Works
+
+seeCharacterInventory()
